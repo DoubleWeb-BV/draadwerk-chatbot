43e9549c67921d1998a081ce9e91c06097a9439a
Tim Meeuwsen <tim@doubleweb.nl>
2025-08-26 15:23:55 +0200

    Update chatbot

--- chatbot-loader.js
+++ chatbot-loader.js
@@ -3,10 +3,7 @@
 
     // Stap 1: Bepaal het script dat zichzelf aanroept
     const currentScript = document.currentScript || [...document.scripts].pop();
-    const scriptSrc = currentScript.src;
-    const versionMatch = scriptSrc.match(/@([^/]+)\/chatbot-loader\.js/);
-    const version = versionMatch ? versionMatch[1] : 'latest';
-    const baseCDN = `https://cdn.jsdelivr.net/gh/DoubleWeb-BV/draadwerk-chatbot@${version}/`;
+    const baseCDN = `https://cdn.jsdelivr.net/gh/DoubleWeb-BV/draadwerk-chatbot/`;
 
     const cssURL  = `${baseCDN}chat.css?ts=${timestamp}`;
     const htmlURL = `${baseCDN}chat.html?ts=${timestamp}`;
